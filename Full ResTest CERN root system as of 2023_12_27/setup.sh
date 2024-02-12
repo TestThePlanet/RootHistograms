97d9181c5d8b906670b3c1256ada6af6a2e939d1
fiveisgreen <4203265+fiveisgreen@users.noreply.github.com>
2024-02-12 12:06:08 +0100

    got a start on the setup script

--- Full ResTest CERN root system as of 2023_12_27/setup.sh
+++ Full ResTest CERN root system as of 2023_12_27/setup.sh
@@ -2,17 +2,15 @@ chmod +x ./go.sh
 chmod +x ./makeAllPlots_nocrop.sh
 chmod +x ./makeAllPlots.sh
 chmod +x ./send.sh
-alias commit='source "$(pwd)"/commit.sh'
 alias go='source "$(pwd)"/go.sh'
 alias save='source "$(pwd)"/send.sh'
 alias send='source "$(pwd)"/send.sh'
-alias commit='source `pwd`/commit.sh'
 if [ $(grep -c "^[^#]alias go=[^#]go.sh" ~/.bashrc) -eq 1 ]; then
-    echo "alias go='source /go.sh'" >> ~/.bashrc
+    echo "alias go='source \"$(pwd)\"/go.sh'" >> ~/.bashrc
 fi
 if [ $(grep -c "^[^#]alias save=[^#]send.sh" ~/.bashrc) -eq 1 ]; then
-    echo "alias save='source /send.sh'" >> ~/.bashrc
+    echo "alias save='source \"$(pwd)\"/send.sh'" >> ~/.bashrc
 fi
 if [ $(grep -c "^[^#]alias send=[^#]send.sh" ~/.bashrc) -eq 1 ]; then
-    echo "alias send='source /send.sh'" >> ~/.bashrc
+    echo "alias send='source \"$(pwd)\"/send.sh'" >> ~/.bashrc
 fi
