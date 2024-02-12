c4432157771b606c9960800b9bdfe6132c5317c4
fiveisgreen <4203265+fiveisgreen@users.noreply.github.com>
2024-02-12 12:00:05 +0100

    :x

--- Full ResTest CERN root system as of 2023_12_27/go.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Doing git pull"
-git pull
-source makeAllPlots_nocrop.sh
-echo "\nMaking Overlays"
-python3 make2Overlays.py
-
-
---
+++ Full ResTest CERN root system as of 2023_12_27/setup.sh
@@ -0,0 +1,16 @@
+chmod +x ./go.sh
+chmod +x ./makeAllPlots_nocrop.sh
+chmod +x ./makeAllPlots.sh
+chmod +x ./send.sh
+alias go='source `pwd`/go.sh'
+alias save='source `pwd`/send.sh'
+alias send='source `pwd`/send.sh'
+if [ $(grep -c "^[^#]alias go=[^#]go.sh" ~/.bashrc) -eq 1 ]; then
+    echo "alias go='source `pwd`/go.sh'" >> ~/.bashrc
+fi
+if [ $(grep -c "^[^#]alias save=[^#]send.sh" ~/.bashrc) -eq 1 ]; then
+    echo "alias save='source `pwd`/send.sh'" >> ~/.bashrc
+fi
+if [ $(grep -c "^[^#]alias send=[^#]send.sh" ~/.bashrc) -eq 1 ]; then
+    echo "alias send='source `pwd`/send.sh'" >> ~/.bashrc
+fi
