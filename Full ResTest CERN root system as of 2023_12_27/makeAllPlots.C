080a357ddf75de830a7a4197063c7895a68d8f7d
fiveisgreen <4203265+fiveisgreen@users.noreply.github.com>
2024-02-16 14:34:27 +0100

    Edited timestamp to single line

--- Full ResTest CERN root system as of 2023_12_27/makeAllPlots.C
+++ Full ResTest CERN root system as of 2023_12_27/makeAllPlots.C
@@ -702,10 +702,10 @@ void PlotAndSave(Hist* hist, TF2* grad, string fname_noext){
 
     //Display timestamp
     TPaveText *timestamp1= new TPaveText( 
-            0.00120627, 0.939834, 0.124246, 0.997925,
+            0.00120627, 0.965768, 0.179131, 0.997925,
             "NDC");
    // timestamp1->AddText("TIL score distribution as of ");
-    timestamp1->AddText("TIL score distribution as of " getCurrentDateTime().c_str());
+    timestamp1->AddText(("TIL as of " + getCurrentDateTime()).c_str());
     PrettyPaveText(timestamp1);
     timestamp1->SetTextAlign(12); //Align to top left
     timestamp1->Draw();
