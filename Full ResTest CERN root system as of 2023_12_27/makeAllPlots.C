999d95740b159f933b915d727854e39c9d21bbb7
TestThePlanet <150461128+TestThePlanet@users.noreply.github.com>
2024-03-15 12:52:40 -0400

    Alterations committed Fri Mar 15 12:52:40 EDT 2024 by Ilya

--- Full ResTest CERN root system as of 2023_12_27/makeAllPlots.C
+++ Full ResTest CERN root system as of 2023_12_27/makeAllPlots.C
@@ -105,7 +105,7 @@ bool lut1_uses_harmean = true; //false = uses median.
 //also uses red_hex and yellow_hex
 
 //trafficLight Color Scheme Constants
-static const char* red_hex = "#7F7F7F"; // formerly "#FF3355"; then, "#FF8A80" // ilya //#7F7F7F
+static const char* red_hex = "#C3C3C3"; // formerly "#FF3355"; then, "#FF8A80" // ilya //#7F7F7F
 static const char* yellow_hex = "#C3C3C3";// formerly "#FFAA00"; then #FFCDAB // ilya 
 static const char* green_hex = "#AFFFAB";// formerly "#26E600"; // "##AFFFAB";
 static const float red_end = TMath::Log10(10.);
