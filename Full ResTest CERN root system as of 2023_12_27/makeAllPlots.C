404d6e441d6c454b64c76f12890e16a8b41f3a1a
TestThePlanet <150461128+TestThePlanet@users.noreply.github.com>
2024-03-15 08:32:23 -0400

    Alterations committed Fri Mar 15 08:32:23 EDT 2024 by Ilya

--- Full ResTest CERN root system as of 2023_12_27/makeAllPlots.C
+++ Full ResTest CERN root system as of 2023_12_27/makeAllPlots.C
@@ -112,8 +112,8 @@ static const float red_end = TMath::Log10(10.);
 static const float yellow_end = TMath::Log10(30.);
 
 //trafficLightFaded Color Scheme Constants 
-static const char* red_hex_TLF = "#FF3355";
-static const char* yellow_hex_TLF = "#FFAA00";
+static const char* red_hex_TLF = "#7F7F7F";
+static const char* yellow_hex_TLF = "#C3C3C3";
 static const char* green_hex_TLF = "#26E600";
 static const float red_end_TLF = TMath::Log10(10.);
 static const float yellow_end_TLF = TMath::Log10(30.);
